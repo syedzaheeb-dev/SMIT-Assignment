72d5989cd2853bca42fa6065a9e6ea3962b5a843
Syed Zaheeb <syedzaheeb.kazmi@gmail.com>
2026-06-21 00:15:08 +0500

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,62 @@
+var age = 11
+var height = 134
+var number_of_toys = 9
+var lucky_number = 7
+var secret_code = 1234
+var favorite_number = 3
+
+// 1
+if (age >10) {
+    console.log("You can go to the big games!")
+}else{
+    console.log("You can not go to big games!")
+}
+
+// 2
+if (age <5) {
+    console.log("Ask your parents to come with you")
+}else{
+    console.log("You are Allowed to come alone.")
+}
+
+// 3
+if (height >=120) {
+    console.log("You can ride the roller coaster!")
+}else{
+    console.log("You can not ride the roller coaster!")
+}
+
+// 4
+if (number_of_toys <=10) {
+    console.log("Your toy box has enough space.")
+}else{
+    console.log("Your toy box has enough space.")
+}
+
+// 5
+if (lucky_number === 7){
+    console.log("Lucky number matched!")
+}else{
+    console.log("Lucky number didn't matched!")
+}
+
+// 6
+if (secret_code === 1234) {
+    console.log("Correct code!")
+}else{
+    console.log("Incorrect code!")
+}
+
+// 7
+if (age !=8) {
+    console.log("You are not 8 years old.")
+}else {
+    console.log("You are 8 years old.")
+}
+
+// 7
+if (favorite_number !==5) {
+    console.log("Favorite number is not exactly 5.")
+}else{
+    console.log("Favorite number is exactly 5.")
+}
