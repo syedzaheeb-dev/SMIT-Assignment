f83c7057d1b2a063bd6b6f063a53c2c833f9cbc9
Syed Zaheeb <syedzaheeb.kazmi@gmail.com>
2026-06-21 00:16:01 +0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,55 @@
+age = 9
+height = 134
+number_of_toys = 8
+lucky_number = 7
+secret_code = 1234
+favorite_number = 3
+
+
+# 1 
+if age>10:
+    print("You can go to the big games!")
+else:
+    print("You can't go to big games!")
+
+#  2
+if age <5:
+    print("Ask your parents to come with you.")
+else:
+    print("You are allowed to come alone")
+
+# 3 
+if height >=120:
+    print("You can ride the roller coaster!")
+else:
+    print("You can't ride the roller coaster!")
+
+# 4 
+if number_of_toys <=10:
+    print("Your toy box has enough space.")
+else:
+    print("Your toy box is out of space.")
+
+# 5 
+if lucky_number == 7:
+    print("Lucky number matched!")
+else:
+    print("Lucky number didn't matched!")
+
+# 6 
+if secret_code == 1234:
+    print("Correct code!")
+else:
+    print("Incorrect code!")
+
+# 7
+if age !=8:
+    print("You are Not 8 Years Old")
+else:
+    print("You are 8 Years Old") 
+
+# 8
+if favorite_number !=5:
+    print("Favorite number is not exactly 5.")
+else:
+    print("Favorite number is exactly 5.")
